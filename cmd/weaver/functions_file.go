7243d4ac587644a3df27614252a1590418b18e97
grantseltzer <grantseltzer@gmail.com>
2020-02-28 22:18:14 -0500

    Fix bug where empty lines in functions file caused empty traceContext objects to surface in array of traceContexts
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- cmd/weaver/functions_file.go
+++ cmd/weaver/functions_file.go
@@ -14,25 +14,30 @@ func read_functions_file(path string) ([]functionTraceContext, error) {
 	}
 
 	functionStringsToTrace := strings.Split(string(content), "\n")
-	contexts := make([]functionTraceContext, len(functionStringsToTrace))
 
-	for i, funcString := range functionStringsToTrace {
+	contexts := []functionTraceContext{}
 
-		if funcString == "" {
+	for _, funcString := range functionStringsToTrace {
+
+		if funcString == "" || funcString == "\n" {
 			continue
 		}
 
 		debugLog("parsing: %s\n", funcString)
 
-		err := parseFunctionAndArgumentTypes(&contexts[i], funcString)
+		newContext := functionTraceContext{}
+
+		err := parseFunctionAndArgumentTypes(&newContext, funcString)
 		if err != nil {
 			return nil, fmt.Errorf("could not parse function string '%s': %s", funcString, err.Error())
 		}
 
-		err = determineStackOffsets(&contexts[i])
+		err = determineStackOffsets(&newContext)
 		if err != nil {
 			return nil, fmt.Errorf("could not determine stack offsets of arguments: %s", err.Error())
 		}
+
+		contexts = append(contexts, newContext)
 
 	}
 
