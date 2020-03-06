4224baff67b3be926307605c14f9309f1d3ac00e
fabien <fama@kaloom.com>
2020-03-06 11:44:12 -0500

    searching in /proc/

--- cmd/weaver/pid.go
+++ cmd/weaver/pid.go
@@ -2,43 +2,21 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"os"
-	"syscall"
 )
-
-func getProcessRunningStatus(pid int) (*os.Process, error) {
-	proc, err := os.FindProcess(pid)
-	if err != nil {
-		return nil, err
-	}
-
-	//double check if process is running and alive
-	//by sending a signal 0
-	//NOTE : syscall.Signal is not available in Windows
-
-	err = proc.Signal(syscall.Signal(0))
-	if err == nil {
-		return proc, nil
-	}
-
-	if err == syscall.ESRCH {
-		return nil, errors.New("process not running")
-	}
-
-	// default
-	return nil, errors.New("process running but query operation not permitted")
-}
 
 func getBinaryFromPID(pid int) (string, error) {
 	// Weaver should have the rights to read /proc
-
-	// Check if process is running
-	_, err := getProcessRunningStatus(pid)
+	exe := fmt.Sprintf("/proc/%d/exe", pid)
+	_, err := os.Stat(exe)
 	if err != nil {
+		if os.IsNotExist(err) {
+			return "", errors.New("process not running")
+		}
+
 		return "", err
 	}
 
-	// Get executable name
-
-	return "", nil
+	return exe, nil
 }
