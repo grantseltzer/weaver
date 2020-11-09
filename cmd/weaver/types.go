9fa639b95b293d179d221cd02eece7861ed85be7
grantseltzer <grantseltzer@gmail.com>
2020-11-08 21:22:48 -0500

    delete unneeded files
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- cmd/weaver/types.go
+++ cmd/weaver/types.go
@@ -6,43 +6,6 @@ import (
 	"fmt"
 )
 
-type filters struct {
-	Pid uint32
-}
-
-type functionTraceContext struct {
-	binaryName   string
-	Filters      filters
-	FunctionName string
-	HasArguments bool       // used for parsing text template
-	Arguments    []argument `json:",omitempty"`
-}
-
-type argument struct {
-	CType          string
-	goType         goType
-	StartingOffset int
-	VariableName   string
-	PrintfFormat   string
-	TypeSize       int
-	ArrayLength    int // Set as 0 if not array
-	IsSlice        bool
-	IsPointer      bool
-}
-
-type procInfo struct {
-	Pid  uint32 `json:"pid,omitempty"`
-	Ppid uint32 `json:"ppid,omitempty"`
-	Comm string `json:"comm,omitempty"`
-}
-
-type modeOfOperation uint8
-
-const (
-	PACKAGE_MODE   modeOfOperation = 1
-	FUNC_FILE_MODE modeOfOperation = 2
-)
-
 // unmarshalBinary for procInfo
 func (i *procInfo) unmarshalBinary(data []byte) error {
 
--- tests/README.md
+++
@@ -1,8 +0,0 @@
-To generate new smoke test test_data:
-
-- As functions are added to cmd/tester/main.go:
-    - make sure they're called in main
-    - add them to tests/test_data/functions_file.txt
-
-- Run weaver, and then the tester. 
-- Pipe output to a file. jq -s -c sorted by function name, saved to tester_output_sorted_slurped.json
