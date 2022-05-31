a553e8319a06bf3057b84503c146e31373724b37
grantseltzer <grantseltzer@gmail.com>
2022-05-31 14:56:28 -0400

    remove old vendoring
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- cmd/print-stack/main.go
+++ cmd/print-stack/main.go
@@ -1,77 +1,77 @@
 package main
 
-import (
-	"encoding/binary"
-	"fmt"
-	"log"
-	"os"
-	"os/signal"
+// import (
+// 	"encoding/binary"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"os/signal"
 
-	bpf "github.com/iovisor/gobpf/bcc"
-)
+// 	bpf "github.com/iovisor/gobpf/bcc"
+// )
 
-const textTemplate = `
-	#include <uapi/linux/ptrace.h>
+// const textTemplate = `
+// 	#include <uapi/linux/ptrace.h>
 
-	BPF_PERF_OUTPUT(events);
+// 	BPF_PERF_OUTPUT(events);
 
-	inline int print_stack(struct pt_regs *ctx) {
-		void* stackAddr = (void*)ctx->sp;
+// 	inline int print_stack(struct pt_regs *ctx) {
+// 		void* stackAddr = (void*)ctx->sp;
 
-		unsigned int slice;
-		bpf_probe_read(&slice, sizeof(slice), stackAddr+8); 
-		bpf_trace_printk("0x%x\n", slice);
-		bpf_trace_printk("0x%x\n", stackAddr);
+// 		unsigned int slice;
+// 		bpf_probe_read(&slice, sizeof(slice), stackAddr+8);
+// 		bpf_trace_printk("0x%x\n", slice);
+// 		bpf_trace_printk("0x%x\n", stackAddr);
 
-		int i;
-		char y;
-		for (i = 0; i < 50; i++) {
-			char *x = (char *)stackAddr+i;
-			bpf_probe_read(&y, sizeof(y), x);
-			events.perf_submit(ctx, &y, sizeof(y));
-		}
+// 		int i;
+// 		char y;
+// 		for (i = 0; i < 50; i++) {
+// 			char *x = (char *)stackAddr+i;
+// 			bpf_probe_read(&y, sizeof(y), x);
+// 			events.perf_submit(ctx, &y, sizeof(y));
+// 		}
 
-		return 0;
-	}
-`
+// 		return 0;
+// 	}
+// `
 
-func main() {
-	bpfModule := bpf.NewModule(textTemplate, []string{})
-	defer bpfModule.Close()
+// func main() {
+// 	bpfModule := bpf.NewModule(textTemplate, []string{})
+// 	defer bpfModule.Close()
 
-	uprobeFd, err := bpfModule.LoadUprobe("print_stack")
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	uprobeFd, err := bpfModule.LoadUprobe("print_stack")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	err = bpfModule.AttachUprobe(os.Args[1], os.Args[2], uprobeFd, -1)
-	if err != nil {
-		log.Fatalf("could not attach uprobe to symbol: %s: %s", "test_function", err.Error())
-	}
+// 	err = bpfModule.AttachUprobe(os.Args[1], os.Args[2], uprobeFd, -1)
+// 	if err != nil {
+// 		log.Fatalf("could not attach uprobe to symbol: %s: %s", "test_function", err.Error())
+// 	}
 
-	// Set up bpf perf map to use for output
-	table := bpf.NewTable(bpfModule.TableId("events"), bpfModule)
-	channel := make(chan []byte)
+// 	// Set up bpf perf map to use for output
+// 	table := bpf.NewTable(bpfModule.TableId("events"), bpfModule)
+// 	channel := make(chan []byte)
 
-	perfMap, err := bpf.InitPerfMap(table, channel)
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	perfMap, err := bpf.InitPerfMap(table, channel)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, os.Kill)
+// 	sig := make(chan os.Signal, 1)
+// 	signal.Notify(sig, os.Interrupt, os.Kill)
 
-	go func() {
-		i := 0
-		for {
-			value := <-channel
-			x1 := binary.LittleEndian.Uint32(value)
-			fmt.Printf("SP+%d:\t%d (0x%x)\n", i, x1, x1)
-			i++
-		}
-	}()
+// 	go func() {
+// 		i := 0
+// 		for {
+// 			value := <-channel
+// 			x1 := binary.LittleEndian.Uint32(value)
+// 			fmt.Printf("SP+%d:\t%d (0x%x)\n", i, x1, x1)
+// 			i++
+// 		}
+// 	}()
 
-	perfMap.Start()
-	<-sig
-	perfMap.Stop()
-}
+// 	perfMap.Start()
+// 	<-sig
+// 	perfMap.Stop()
+// }
--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
 module github.com/grantseltzer/weaver
 
-go 1.13
-
-require (
-	github.com/iovisor/gobpf v0.0.0-20200227201708-df376f8c6350
-	github.com/stretchr/testify v1.5.1
-	github.com/urfave/cli/v2 v2.1.1
-)
+go 1.16
